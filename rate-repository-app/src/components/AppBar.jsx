d0eaaffc4690cea5a3e27edf73d918ee088e2188
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-12-15 14:22:54 +0200

    10.7

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
@@ -12,6 +12,8 @@ const styles = StyleSheet.create({
     paddingBottom: Constants.statusBarHeight,
     flexDirection: 'row',
     justifyContent: 'flex-start',
+    paddingLeft: 10
+
     
     // ...
   },
@@ -29,7 +31,7 @@ const styles = StyleSheet.create({
 const AppBarTab = ({name, link}) => {
   return (
     <View style={styles.tab}>
-      <Link to={link}>
+      <Link to={link} component={TouchableOpacity} activeOpacity={0.8}>
         <Text style={styles.text}>{name}</Text>
       </Link>
     </View>
@@ -38,12 +40,14 @@ const AppBarTab = ({name, link}) => {
 
 const AppBar = () => {
   return (
-  <TouchableWithoutFeedback onPress={() => alert('Pressed!')}>
+  //<TouchableWithoutFeedback onPress={() => alert('Pressed!')}>
     <View style={styles.container}>
-      <AppBarTab name="Repositories" link='/'/>
-      <AppBarTab name="Sign in" link='/signin'/>
+      <ScrollView horizontal>
+        <AppBarTab name="Repositories" link='/'/>
+        <AppBarTab name="Sign in" link='/signin'/>
+      </ScrollView>
     </View>
-  </TouchableWithoutFeedback>
+  //</TouchableWithoutFeedback>
   );
 };
 
