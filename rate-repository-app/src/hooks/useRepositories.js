107e690d93cb36fdbca2879461487c7cec3a0b00
Jussi-Pekka Parkkari <j-p.parkkari@iki.fi>
2020-12-18 11:44:01 +0200

    10.11 fixed

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,33 +1,15 @@
-//import { useState, useEffect } from 'react';
+
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  //const [repositories, setRepositories] = useState();
-  //const [loading, setLoading] = useState(false);
-
-  const { loading, data } = useQuery(GET_REPOSITORIES, {
+  
+  const { loading, error, data } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
   });
+  
     
-  /*
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    // Replace the IP address part with your own IP address!
-    const response = await fetch('http://192.168.1.86:5000/api/repositories');
-    
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-  */  
-  return { repositories: data.repositories, loading};//, refetch: fetchRepositories };
+  return { repositories: data ? data.repositories : undefined, loading, error };
 };
 
 export default useRepositories;
